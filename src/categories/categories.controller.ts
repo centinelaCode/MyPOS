0b4ffb5f7e89650d5be9cb7ada84639ad6fa1d1f
centinelacode <centinela1024tb@gmail.com>
2025-12-28 21:39:15 -0600

    [ADD] configuracion de los DTOs para cambiar la respuesta por defecto

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,6 +9,8 @@ export class CategoriesController {
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
+    // console.log(createCategoryDto)
+
     return this.categoriesService.create(createCategoryDto);
   }
 
