cd639c1cfb7ebd0124a0c9c3dc0ecc7c1ca08a4e
centinelacode <centinela1024tb@gmail.com>
2025-12-31 15:07:43 -0600

    [ADD] agregamos el constructror de la categoryRepository

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
+  constructor(
+    @InjectRepository(Category) private readonly categoryRepository : Repository<Category>
+  ) {}
+
+  create(createCategoryDto: CreateCategoryDto) {    
     return `This action adds a new category`;
   }
 
