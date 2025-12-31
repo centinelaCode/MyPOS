d8f598c570fd722d5dba06eeb5a0c6e3a67467da
centinelacode <centinela1024tb@gmail.com>
2025-12-31 15:10:30 -0600

    [ADD] addconstructor al category.Services y en catgory.moduules agregamos imports el TypeOrm Module (Category)

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -10,7 +10,6 @@ export class CategoriesController {
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     // console.log(createCategoryDto)
-    console.log(process.env.DATABASE_HOST)
 
     return this.categoriesService.create(createCategoryDto);
   }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -12,13 +12,11 @@ export class CategoriesService {
   ) {}
 
   create(createCategoryDto: CreateCategoryDto) {    
-    return `This action adds a new category`;
   }
 
   findAll() {
     return `This action returns all categories`;
   }
-
   findOne(id: number) {
     return `This action returns a #${id} category`;
   }
