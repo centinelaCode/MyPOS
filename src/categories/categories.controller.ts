3997e4eae4ba38773857e950dd09ccdd40211737
Centinela Code <centinela1024tb@gmail.com>
2026-01-02 20:13:51 -0600

    [ADD] creamos un PIPE para validar que param que se pasa en la categoria sea un number, si no mostrar el error

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, BadRequestException } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -20,7 +20,7 @@ export class CategoriesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', new ParseIntPipe({exceptionFactory: () => new BadRequestException('ID no valido')})) id: string) {
     return this.categoriesService.findOne(+id);
   }
 
